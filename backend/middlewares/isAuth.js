e41d63e2e36019a4cb9dcd526990ee77309c6d1b
TusharFodse <tusharfodse@gmail.com>
2024-10-21 00:55:53 +0530

    Update isAuth.js

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -6,6 +6,7 @@ import {User} from '../models/userModel.js'
 export const isAuth= async (req,res,next) =>{
     try {
         const token =req.cookies.token;
+         console.log("Token received:", token);
 
         if(!token)  
             return res.status(403).json({
@@ -31,6 +32,7 @@ export const isAuth= async (req,res,next) =>{
               
     } catch (error) {
         res.status(500).json({
+            console.error("Error in isAuth middleware:", error);
             message:"Please Login"
         })
         
